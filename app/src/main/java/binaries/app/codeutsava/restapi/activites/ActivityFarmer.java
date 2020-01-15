17d173f33efd54d20d3c101be63023e079e564a9
naveensundar <naveennvrgup@gmail.com>
2020-01-15 19:21:04 +0530

    temp commit

--- app/src/main/java/binaries/app/codeutsava/restapi/activites/ActivityFarmer.java
+++ app/src/main/java/binaries/app/codeutsava/restapi/activites/ActivityFarmer.java
@@ -11,5 +11,7 @@ public class ActivityFarmer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer);
+
+
     }
 }
--- app/src/main/java/binaries/app/codeutsava/restapi/fragments/FragmentFarmingInfo.java
+++ app/src/main/java/binaries/app/codeutsava/restapi/fragments/FragmentFarmingInfo.java
@@ -1,4 +1,4 @@
-package binaries.app.codeutsava.restapi.activites;
+package binaries.app.codeutsava.restapi.fragments;
 
 
 import android.os.Bundle;
@@ -14,10 +14,10 @@ import binaries.app.codeutsava.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentFarmerBids extends Fragment {
+public class FragmentFarmingInfo extends Fragment {
 
 
-    public FragmentFarmerBids() {
+    public FragmentFarmingInfo() {
         // Required empty public constructor
     }
 
@@ -26,7 +26,7 @@ public class FragmentFarmerBids extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_farmer_bids, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 
 }
