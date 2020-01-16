7223afdbc2d27e1180f482451efc2502995d993c
naveensundar <naveennvrgup@gmail.com>
2020-01-16 12:31:10 +0530

    bottom sheet nav done

--- app/src/main/java/binaries/app/codeutsava/restapi/activites/ActivityFarmer.java
+++ app/src/main/java/binaries/app/codeutsava/restapi/activites/ActivityFarmer.java
@@ -12,24 +12,18 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import binaries.app.codeutsava.R;
-import binaries.app.codeutsava.restapi.fragments.FragmentFarmerBids;
-import binaries.app.codeutsava.restapi.fragments.FragmentFarmerDetail;
-import binaries.app.codeutsava.restapi.fragments.FragmentFarmerMenu;
-import binaries.app.codeutsava.restapi.fragments.FragmentFarmerProduce;
-
-import static com.google.android.material.bottomsheet.BottomSheetBehavior.from;
+import binaries.app.codeutsava.restapi.fragments.FragmentFarmerBottomSheet;
 
 public class ActivityFarmer extends AppCompatActivity {
 
     ImageView menuButton;
-    boolean menuVisible=false;
-    LinearLayout farmermenuLayout;
-    LinearLayout farmerMenuUserData;
-    RecyclerView farmerMenuRecycler;
-    BottomSheetBehavior bottomSheetBehavior;
-    RelativeLayout bottomSheetHolderRL;
+    BottomSheetBehavior sheetBehavior;
+    RelativeLayout bottomSheet;
+
+
 
 
     @Override
@@ -37,56 +31,21 @@ public class ActivityFarmer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer);
 
-//        FragmentFarmerDetail farmerDetail = new FragmentFarmerDetail();
-//        FragmentFarmerBids farmerBids = new FragmentFarmerBids();
-//        FragmentFarmerProduce frament = new FragmentFarmerProduce();
-//        FragmentFarmerMenu farmerMenu = new FragmentFarmerMenu();
-//        getSupportFragmentManager().beginTransaction()
-//                .add(R.id.farmerDashboard, farmerMenu)
-//                .commit();
+
+        bottomSheet = findViewById(R.id.farmerBottomSheet);
+        sheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        menuButton=findViewById(R.id.farmerMenuIcon);
 
 
-        LinearLayout bottom_sheet = findViewById(R.id.bottom_sheet);
-        BottomSheetBehavior sheetBehavior=BottomSheetBehavior.from(bottom_sheet);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-//        menuButton = (ImageView)findViewById(R.id.menu_icon);
-//        farmermenuLayout = (LinearLayout) findViewById(R.id.bottomFarmerMenu);
-////        farmerMenuRecycler = (RecyclerView) findViewById(R.id.farmerMenuRecyclerView);
-//        farmerMenuUserData = (LinearLayout) findViewById(R.id.farmerMenuUserData);
-//        bottomSheetHolderRL = findViewById(R.id.bottomSheetHolderRL);
-//
-//        bottomSheetBehavior= BottomSheetBehavior.from(bottomSheetHolderRL);
+                BottomSheetDialogFragment bottomSheetDialogFragment=new FragmentFarmerBottomSheet();
+                bottomSheetDialogFragment.show(getSupportFragmentManager(),"farmerBottomSheet");
+            }
+        });
 
-//        menuButton.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View v) {
-//                String bl;
-//                if(menuVisible){
-//                    menuVisible=false;
-//                    bl = "false";
-//                }
-//                else{
-//                    menuVisible=true;
-//                    bl= "true";
-//                }
-//                Toast.makeText(getActivity(), "This is my Toast message! "+bl,
-//                        Toast.LENGTH_LONG).show();
-//            }
-//        });
-//
-//
-//        if(menuVisible){
-//            farmermenuLayout.setVisibility(View.VISIBLE);
-//            farmerMenuRecycler.setVisibility(View.VISIBLE);
-//            farmerMenuUserData.setVisibility(View.VISIBLE);
-//
-//        }
-//        else{
-//            farmermenuLayout.setVisibility(View.GONE);
-//            farmerMenuRecycler.setVisibility(View.GONE);
-//            farmerMenuUserData.setVisibility(View.GONE);
-//        }
 
     }
 
