c23f83c46f12b215cf506ff2ebf49706d8c27599
ethicalrushi <pupalerushikesh@gmail.com>
2020-01-17 09:57:30 +0530

    commit till warehouse

--- app/src/main/java/binaries/app/codeutsava/restapi/fragments/FragmentFarmerFindWarehouse.java
+++ app/src/main/java/binaries/app/codeutsava/restapi/fragments/FragmentFarmerFindWarehouse.java
@@ -96,33 +96,3 @@ public  class  FragmentFarmerFindWarehouse extends DialogFragment {
     }
 
 }
-
-
-
-
-
-
-//public class FragmentFarmerMenu extends Fragment {
-//    RecyclerView recyclerView;
-//    AdapterFarmerMenu mAdapter;
-//
-//    public  FragmentFarmerMenu(){
-//
-//    }
-//
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View view = inflater.inflate(R.layout.activity_farmer, container, false);
-//        recyclerView = view.findViewById(R.id.farmerMenuRecyclerView);
-//
-//        mAdapter = new AdapterFarmerMenu(getContext());
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//        recyclerView.setAdapter(mAdapter);
-//        mAdapter.notifyDataSetChanged();
-//        return view;
-//    }
-//
-//}
